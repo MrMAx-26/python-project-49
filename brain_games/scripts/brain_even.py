639889c29b9360e0afe1b6239ab3fdb9d2120d7d
Максим Шевцов <shevtsov_stv@mail.ru>
2024-12-14 11:11:33 +0300

    Add brain_even game

---
+++ brain_games/scripts/brain_even.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+
+def main():
+    from random import randint
+    from prompt import string
+    from brain_games import cli
+    print('Welcome to the Brain Games!')
+    name = cli.welcome_user()
+    print('Answer "yes" if the number is even, otherwise answer "no".')
+    i = 0
+    while i < 3:
+        question = randint(0, 100)
+        correct_answer = 'yes' if is_even(question) else 'no'
+        print(f'Question: {question}')
+        answer = string('Your answer: ')
+        if correct_answer == answer:
+            print('Correct!')
+            i += 1
+        else:
+            i = 4
+    if i == 4:
+        print(f"'{answer}' is wrong answer ;(. Correct answer was '{correct_answer}'.")
+    if i == 3:
+        print(f'Congratulations, {name}!')
+
+
+def is_even(question: int) -> bool:
+    return True if question % 2 == 0 else False
+
+
+if __name__ == '__main__':
+    main()
