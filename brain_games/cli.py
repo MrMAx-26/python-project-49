1ba81c9538e0af6f3db74b071c643b6596e88edb
Максим Шевцов <shevtsov_stv@mail.ru>
2024-12-14 11:12:03 +0300

    Update for brain_even

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -3,3 +3,8 @@ def welcome_user():
     from prompt import string
     name = string('May I have your name? ')
     print(f'Hello, {name}!')
+    return name
+    
+    
+
+
--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -6,6 +6,8 @@ def main():
     from brain_games import cli
     print('Welcome to the Brain Games!')
     cli.welcome_user()
+    
+   
 
 
 if __name__ == '__main__':
