9bf271137316e6b6080edf401f1a858856944548
Максим Шевцов <shevtsov_stv@mail.ru>
2024-12-16 16:20:04 +0300

    Fix linter error

--- brain_games/calc_game.py
+++ brain_games/calc_game.py
@@ -14,6 +14,7 @@ def play_calc_game():
         number1, number2 = randint(0, 25), randint(0, 25)
         print(f'Question: {number1} {operator} {number2}')
         answer = int(string("Your answer: "))
-        correct_answer = engine.get_correct_math_answer(number1, number2, operator)
+        correct_answer = engine.get_correct_math_answer(number1, number2, 
+            operator)
         i = engine.check_answer(answer, correct_answer, i)
     engine.print_result_of_game(answer, correct_answer, name, i)
