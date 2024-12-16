9e1e8e859a36b8cbe8ce31a5e12dfadc83463382
Максим Шевцов <shevtsov_stv@mail.ru>
2024-12-16 15:00:26 +0300

    Refactoring brain_even

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -2,30 +2,9 @@
 
 
 def main():
-    from random import randint
+    from brain_games import even_game
 
-    from prompt import string
-
-    from brain_games import engine
-    name = engine.greet_and_get_name()
-    print('Answer "yes" if the number is even, otherwise answer "no".')
-    i = 0
-    while i < 3:
-        question = randint(0, 100)
-        correct_answer = 'yes' if is_even(question) else 'no'
-        print(f'Question: {question}')
-        answer = string('Your answer: ')
-        i = engine.check_answer(answer, correct_answer, i)
-    if i == 4:
-        print(f"'{answer}' is wrong answer ;(. Correct answer was " + 
-            f"'{correct_answer}'.")
-        print(f"Let's try again, {name}!")
-    if i == 3:
-        print(f'Congratulations, {name}!')
-
-
-def is_even(question: int) -> bool:
-    return True if question % 2 == 0 else False
+    even_game.play_even_game()
 
 
 if __name__ == '__main__':
