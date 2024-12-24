b7f53184b8eea469a89aaf8b7e7eb6940e1cd156
Максим Шевцов <shevtsov_stv@mail.ru>
2024-12-24 20:48:43 +0300

    Refactoring brain game

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python
 
+from brain_games.engine import greet_and_get_name
+
 
 def main():
-    from brain_games import cli
-
-    print('Welcome to the Brain Games!')
-    cli.welcome_user()
+    greet_and_get_name()
     
 
 if __name__ == '__main__':
